1a7a686073e37e84bb57351a9bfb3694f7845307
Leonomics <leocv97@gmail.com>
2022-07-19 14:43:34 +0200

    create user variables

---
+++ myscript.js
@@ -0,0 +1,7 @@
+let userName = prompt("What's your name?");
+console.log("name: " + userName);
+let userSurname = prompt("What's your surname?");
+console.log("surname: " + userSurname);
+let userFavouriteColor = prompt("What's your favourite color?");
+console.log("color: " + userFavouriteColor);
+const title = document.getElementById('password_title');
